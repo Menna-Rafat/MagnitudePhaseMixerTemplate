a22bd15e8898cf1fd8227f6d1c53c696a8895a0b
Menna El-khatib <43760197+Menna-Rafat@users.noreply.github.com>
2020-04-09 22:47:33 +0200

    code

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from imageModel import ImageModel
 from PyQt5 import QtWidgets , QtGui
 from image import Ui_MainWindow
 import sys
-from  PyQt5.QtWidgets  import QFileDialog ,QVBoxLayout, QPushButton, QLabel, QTextEdit
+from  PyQt5.QtWidgets  import QFileDialog ,QVBoxLayout, QPushButton, QLabel,QSlider, QTextEdit
 import numpy as np
 from PyQt5.QtGui import QPixmap
 import cv2 as cv
@@ -21,23 +21,30 @@ import cv2 as cv
 
 class ApplicationWindow(QtWidgets.QMainWindow):
     def __init__(self):
-        self.data=[]
-        self.y=[]
         super(ApplicationWindow, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-      
+        self.ui.Slider1.setMinimum(0)
+        self.ui.Slider1.setMaximum(100)
+        self.ui.Slider_2.setMinimum(0)
+        self.ui.Slider_2.setMaximum(100)
+        self.ui.Slider1.setTickInterval(10)
+        self.ui.Slider1.setTickPosition(QSlider.TicksBelow)
+        self.ui.Slider_2.setTickInterval(10)
+        self.ui.Slider_2.setTickPosition(QSlider.TicksBelow)
+        self.ui.load1.clicked.connect(self.getImage)
+        self.ui.load2.clicked.connect(self.getImage2)
  
     def getImage(self):
         global fname
         fname = QFileDialog.getOpenFileName(self, 'Open file', '' , "*.jpg *.gif *.png" )[0]
-        global img , base_image
+        global img 
         img = cv.imread(fname,0)
-        dimensions = img.shape
+    
         pixmap = QPixmap(fname)
-        base_image = QtGui.QImage(dimensions[0], dimensions[1], QtGui.QImage.Format_ARGB32)
+        
         self.ui.img1.setPixmap(QPixmap(pixmap))
-        self.resize(pixmap.width(), pixmap.height())
+        
         
     def combox(self):
          comboxes =[self.ui.comboReal1,self.ui.comboReal2,
@@ -51,8 +58,16 @@ class ApplicationWindow(QtWidgets.QMainWindow):
                  self.getReal()
              else: 
                 self.getImaginary()
+    def getImage2(self):
+        global fname
+        fname = QFileDialog.getOpenFileName(self, 'Open file', '' , "*.jpg *.gif *.png" )[0]
+        global img2 
+        img2 = cv.imread(fname,0)
+        
+        pixmap = QPixmap(fname)
+        self.ui.img2.setPixmap(QPixmap(pixmap)) 
            
-    def getMagnitude(self):
+    def getMagnitude(self,string):
         global fshift
         f = np.fft.fft2(img)
         fshift = np.fft.fftshift(f)
@@ -60,26 +75,42 @@ class ApplicationWindow(QtWidgets.QMainWindow):
         
         base_image = QtGui.QImage(magnitude_spectrum.shape[0], magnitude_spectrum.shape[1], QtGui.QImage.Format_ARGB32)
         pixmap = QPixmap(base_image)
-        self.ui.changed_img1.setPixmap(QPixmap(pixmap))
+        if string == self.ui.comboReal1:
+            self.ui.changed_img1.setPixmap(QPixmap(pixmap))
+        elif string == self.ui.comboReal2:
+            self.ui.changed_img2.setPixmap(QPixmap(pixmap))
         
-    def getPhase(self):
+        
+    def getPhase(self,string):
         phase_spectrum = np.angle(fshift)
         pixmap = QPixmap(phase_spectrum)
         self.ui.changed_img1.setPixmap(QPixmap(pixmap))
         base_image = QtGui.QImage(phase_spectrum.shape[0], phase_spectrum.shape[1], QtGui.QImage.Format_ARGB32)
         pixmap = QPixmap(base_image)
-        self.ui.changed_img1.setPixmap(QPixmap(pixmap))  
+        if string == self.ui.comboReal1:
+            self.ui.changed_img1.setPixmap(QPixmap(pixmap))
+        elif string == self.ui.comboReal2:
+            self.ui.changed_img2.setPixmap(QPixmap(pixmap))
+          
      
-    def getReal(self):
+    def getReal(self,string):
         
         real = np.real(fshift)
         pixmap = QPixmap(real)
-        self.ui.changed_img1.setPixmap(QPixmap(pixmap))
+        if string == self.ui.comboReal1:
+            self.ui.changed_img1.setPixmap(QPixmap(pixmap))
+        elif string == self.ui.comboReal2:
+            self.ui.changed_img2.setPixmap(QPixmap(pixmap))
         
-    def getImaginary(self):
+        
+    def getImaginary(self,string):
         imag = np.imag(fshift)
         pixmap = QPixmap(imag)
-        self.ui.changed_img1.setPixmap(QPixmap(pixmap))
+        if string == self.ui.comboReal1:
+            self.ui.changed_img1.setPixmap(QPixmap(pixmap))
+        elif string == self.ui.comboReal2:
+            self.ui.changed_img2.setPixmap(QPixmap(pixmap))
+        
 
 class ApplicationWindow(QtWidgets.QMainWindow):
     def __init__(self):
